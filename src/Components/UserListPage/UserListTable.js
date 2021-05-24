20618328400b692106c06f2a6c7157115f3df369
Aravinthan <aravinthanpro@gmail.com>
2021-05-25 00:00:30 +0530

    Handle Empty table case

--- src/Components/UserListPage/UserListTable.js
+++ src/Components/UserListPage/UserListTable.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import UserRowDetails from './UserRowDetails';
 import LocalStoreHelper from '../../Helper/LocalStoreHelper';
-import {UserListContext} from './../../Context/UserListContext';
+import { UserListContext } from './../../Context/UserListContext';
 
 function UserListTable(props) {
     // let userList = [];
@@ -14,35 +14,41 @@ function UserListTable(props) {
 
     // Remove User
     function removeUser(id) {
-        console.log("Remove user of id : "+id);
+        console.log("Remove user of id : " + id);
         LocalStoreHelper.removeUser(id);
         setUserList(LocalStoreHelper.getUsers());
     }
 
-
-
-    return (
-        <div>
-            <h4 id="no-user-table">No User data is added</h4>
-            <table className="table table-striped mt-5" id="user-table">
-                <thead>
-                    <tr>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Age</th>
-                        <th>Email</th>
-                        <th></th>
-                    </tr>
-                    {
-                        userList.map((user) => {
-                            return <UserRowDetails key={user.id} user={user} deleteUser={removeUser} />
-                        })
-                    }
-                </thead>
-                <tbody id="user-list"></tbody>
-            </table>
-        </div>
-    );
+    if (userList.length === 0) {
+        return (
+            <div>
+                <h4 id="no-user-table">No User data is added</h4>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div>
+                <table className="table table-striped mt-5" id="user-table">
+                    <thead>
+                        <tr>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Age</th>
+                            <th>Email</th>
+                            <th></th>
+                        </tr>
+                        {
+                            userList.map((user) => {
+                                return <UserRowDetails key={user.id} user={user} deleteUser={removeUser} />
+                            })
+                        }
+                    </thead>
+                    <tbody id="user-list"></tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 export default UserListTable;
